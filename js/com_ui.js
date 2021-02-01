7f9e748753209485e12f652ccf0871eb57c801a5
termisam005 <termi0004@gmail.com>
2021-02-01 11:00:24 +0900

    commit-0201

---
+++ js/com_ui.js
@@ -0,0 +1,292 @@
+
+/*btn_trans*/
+$(document).ready(function(){
+	$(".btn_trans").click(function(){
+		$(".Dash_body").toggleClass("trans");
+		$(".dash_aside").toggleClass("trans");
+		$(".header_in .logo").toggleClass("trans");
+		$(".left_name").toggleClass("trans");
+		$(".left_menu").toggleClass("trans");
+		$(".top_line").toggleClass("trans");
+		$(".map_content").toggleClass("trans");
+		$(".share_content").toggleClass("trans");
+	})
+});
+
+
+/*Calendar*/
+/*$(document).ready(function(){
+	$(function () {
+		$(".datepicker01").datepicker({ 
+			autoclose: true, 
+			todayHighlight: true,
+			format:"yyyy-mm-dd",
+		}).datepicker('update', new Date());
+		$(".datepicker02").datepicker({ 
+			autoclose: true, 
+			todayHighlight: true,
+			format:"yyyy-mm-dd",
+		}).datepicker('update', new Date());
+	});
+});*/
+
+
+/*left_menu*/
+$( document ).ready(function() {
+	$('.selb_link').click(function(){
+		if(!$(this).hasClass('contact'))
+			$(this).removeClass('contact');
+			$(this).toggleClass('contact');
+			$(this).next(".add_menu").slideToggle().siblings(".add_menu:visible").slideToggle();
+	});	
+});
+
+
+/*toggle button*/
+$(function(){
+	$(".btn_togg").click(function(){
+		$(this).closest(".btbox").toggleClass("on").siblings().removeClass("on");
+	});
+});
+
+
+/*실시간관제 리스트click*/
+$(document).ready(function(){
+	$(".list_conts").click(function(){
+		if ($(".td_check").hasClass("on_check")){
+			$(".td_check").removeClass("on_check");
+			$(this).parents(".td_check").toggleClass("on_check");
+		} else {
+			$(this).parents(".td_check").toggleClass("on_check");
+		}		
+	});
+});
+
+
+/*코인버튼라인*/
+$(document).ready(function(){
+	$(".btn_filt").click(function(){
+		if($(".filt_box").hasClass("slct")){
+			$(".filt_box").removeClass("slct");
+			$(this).parents(".filt_box").toggleClass("slct");
+		} else {
+			$(this).parents(".filt_box").toggleClass("slct");
+		}
+	});
+})
+
+
+/*가운데 마커*/
+$(document).ready(function(){
+	/*$(".img_navi").click(function(){
+		if($(".mark_contents").css("display") == "none"){
+			$(".mark_contents").addClass("on");
+		}
+	});*/
+
+	$(".img_navi").click(function(){
+		$(".mark_contents").toggleClass("on");
+	});
+
+	$(".mark_top .cancel").click(function(){
+		$(".mark_contents").removeClass("on");
+	});
+});
+
+/*오늘쪽리스트와 마커*/
+function rst_mint()
+{
+	$(".img_navi").css('display', 'none');
+	$(".navi_mint").css('display', 'block');
+}
+function rst_pink()
+{
+	$(".img_navi").css('display', 'none');
+	$(".navi_pink").css('display', 'block');
+}
+function rst_blue()
+{
+	$(".img_navi").css('display', 'none');
+	$(".navi_blue").css('display', 'block');
+}
+function rst_gray()
+{
+	$(".img_navi").css('display', 'none');
+	$(".navi_gray").css('display', 'block');
+}
+function rst_yellow()
+{
+	$(".img_navi").css('display', 'none');
+	$(".navi_yellow").css('display', 'block');
+}
+function rst_red()
+{
+	$(".img_navi").css('display', 'none');
+	$(".navi_red").css('display', 'block');
+}
+
+
+/*관제오른쪽,밑에 리스트 토글*/
+$(document).ready(function(){
+	$(".btn_go").click(function(){
+		$(this).parents(".current_show").toggleClass("show");
+	})
+
+	$(".btn_click").click(function(){
+		$(this).parents(".show_wrap").toggleClass("con");
+	});
+})
+
+/*dashboard 탭차트*/
+function tab1()
+{
+	$(".tab_chart2").css('display', 'none');
+	$(".tab_chart1").css('display', 'block');
+	$(".tabt2").removeClass('on');
+	$(".tabt1").addClass('on');
+}
+function tab2()
+{
+	$(".tab_chart1").css('display', 'none');
+	$(".tab_chart2").css('display', 'block');
+	$(".tabt1").removeClass('on');
+	$(".tabt2").addClass('on');
+}
+
+
+/*운행개요*/
+$(document).ready(function(){
+	$(".drive_table1 tbody tr td").click(function(){
+		$(".driving_view").addClass("see");
+
+		if($(".drive_table1 tbody tr").hasClass("select")){
+			$(".drive_table1 tbody tr").removeClass("select");
+			$(this).parents(".drive_table1 tbody tr").addClass("select");
+		} else {
+			$(this).parents(".drive_table1 tbody tr").addClass("select");
+		}
+	});
+})
+
+$(document).ready(function(){
+	$(".drive_table2 tbody tr td").click(function(){
+		if($(".drive_table2 tbody tr").hasClass("select")){
+			$(".drive_table2 tbody tr").removeClass("select");
+			$(this).parents(".drive_table2 tbody tr").addClass("select");
+		} else {
+			$(this).parents(".drive_table2 tbody tr").addClass("select");
+		}
+	});
+})
+
+
+function navtab1()
+{
+	$(".navi_chart02").css('display','none');
+	$(".navi_chart01").css('display','block');
+	$(".navbt2").removeClass('on');
+	$(".navbt1").addClass('on');
+}
+function navtab2()
+{
+	$(".navi_chart01").css('display','none');
+	$(".navi_chart02").css('display','block');
+	$(".navbt1").removeClass('on');
+	$(".navbt2").addClass('on');
+}
+
+
+/*인사관리 탭*/
+function mnge1()
+{
+	$(".manage_conts").css('display','none');
+	$(".maconts1").css('display','block');
+	$(".tab_btn").removeClass('on');
+	$(".mgbtn1").addClass('on');
+}
+function mnge2()
+{
+	$(".manage_conts").css('display','none');
+	$(".maconts2").css('display','block');
+	$(".tab_btn").removeClass('on');
+	$(".mgbtn2").addClass('on');
+}
+function mnge3()
+{
+	$(".manage_conts").css('display','none');
+	$(".maconts3").css('display','block');
+	$(".tab_btn").removeClass('on');
+	$(".mgbtn3").addClass('on');
+}
+function mnge4()
+{
+	$(".manage_conts").css('display','none');
+	$(".maconts4").css('display','block');
+	$(".tab_btn").removeClass('on');
+	$(".mgbtn4").addClass('on');
+}
+function mnge5()
+{
+	$(".manage_conts").css('display','none');
+	$(".maconts5").css('display','block');
+	$(".tab_btn").removeClass('on');
+	$(".mgbtn5").addClass('on');
+}
+function mnge6()
+{
+	$(".manage_conts").css('display','none');
+	$(".maconts6").css('display','block');
+	$(".tab_btn").removeClass('on');
+	$(".mgbtn6").addClass('on');
+}function mnge7()
+{
+	$(".manage_conts").css('display','none');
+	$(".maconts7").css('display','block');
+	$(".tab_btn").removeClass('on');
+	$(".mgbtn7").addClass('on');
+}
+
+
+/*오른쪽 테이블 클릭*/
+$(document).ready(function(){
+	$(".right_table tbody tr td").click(function(){
+		if($(".right_table tbody tr").hasClass("did")){
+			$(".right_table tbody tr").removeClass("did");
+			$(this).parents(".right_table tbody tr").addClass("did");
+		}else{
+			$(this).parents(".right_table tbody tr").addClass("did");
+		}
+	});
+})
+
+
+
+/*관리 검색결과없음 클릭*/
+$(document).ready(function(){
+	$(".gray_table .no_result").click(function(){
+		alert("데이터가 없습니다.");
+	})
+})
+
+/*관리모달 버튼보이기*/
+$(document).ready(function(){
+	$(".btn_step").click(function(){
+		if($(".bt_enroll").css("display") == "none"){
+			$(".bt_repair").hide();
+			$(".bt_enroll").show();
+		}
+	})
+
+	$(".rep_tr").click(function(){
+		if($(".bt_repair").css("display") == "none"){
+			$(".bt_enroll").hide();
+			$(".bt_repair").show();
+		}
+	});
+})
+
+
+/*dropdown*/
+$(document).ready(function(){
+	$(".dropdown-toggle").dropdown();
+});
---
+++ js/datepicker.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+    $(function () {
+        var today = new Date();
+        //today.setHours(1,0,0,0);
+        today.setDate(today.getDate());
+        
+        $('#datetimepicker1').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker2').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker3').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD HH:mm:ss',
+            defaultDate: today,
+        });
+        $('#datetimepicker4').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD HH:mm:ss',
+            defaultDate: today,
+        });
+        $('#datetimepicker5').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker6').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker7').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker8').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker9').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker10').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        
+        $('#datetimepicker11').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker12').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker13').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker14').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker15').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker16').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker17').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker18').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker19').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+        $('#datetimepicker20').datetimepicker({
+            locale: "ko",
+            format: 'YYYY-MM-DD',
+            defaultDate: today,
+        });
+    });
+});    
